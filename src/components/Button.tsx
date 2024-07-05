b7269adc5900f00b46e894f903c1160ceb6e267b
Nazrin Khalilova <nazrinaliyeva0202@gmail.com>
2024-07-05 16:10:28 +0400

    Initial commit of shared components

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button style={{ backgroundColor: "green", color: "white" }}>
+      Helloooo
+    </button>
+  );
+};
+
+export default Button;
